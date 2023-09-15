810b0ce1b9129ddf87b60570ab768b4c3ac3390c
cvillela <csvillela1@gmail.com>
2023-09-15 13:00:16 -0300

    add script

---
+++ audioext/scripts/get_dir_length.py
@@ -0,0 +1,13 @@
+from ..audio.audio_utils import list_wavs_from_dir, get_len_wavs
+import argparse
+
+if __name__ == "__main__":
+    # Create the argument parser
+    parser = argparse.ArgumentParser(description="Parsing Arguments")
+    parser.add_argument("--samples_dir", type=str, default="", help="Path to directory wav samples.")
+    args = parser.parse_args()
+    
+    wavs = list_wavs_from_dir(args.samples_dir)
+    dur = get_len_wavs(wavs)
+    
+    print(f"Directory {args.samples_dir} contains {len(wavs)} files and has a duration of {dur} hours.")
