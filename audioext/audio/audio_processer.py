525a1b6aab0dd6dd2dbfbe5f0eed2bb2c70c6e18
cvillela <csvillela1@gmail.com>
2024-05-14 20:30:40 -0300

    add pcm2float

--- audioext/audio/audio_processer.py
+++ audioext/audio/audio_processer.py
@@ -11,6 +11,36 @@ import itertools
 from .audio_utils import plot_bp_filter
 
 
+def pcm2float(sig, dtype='float32'):
+    """Convert PCM signal to floating point with a range from -1 to 1.
+    Use dtype='float32' for single precision.
+    Parameters
+    ----------
+    sig : array_like
+        Input array, must have integral type.
+    dtype : data type, optional
+        Desired (floating point) data type.
+    Returns
+    -------
+    numpy.ndarray
+        Normalized floating point data.
+    See Also
+    --------
+    float2pcm, dtype
+    """
+    sig = np.asarray(sig)
+    if sig.dtype.kind not in 'iu':
+        raise TypeError("'sig' must be an array of integers")
+    dtype = np.dtype(dtype)
+    if dtype.kind != 'f':
+        raise TypeError("'dtype' must be a floating point type")
+
+    i = np.iinfo(sig.dtype)
+    abs_max = 2 ** (i.bits - 1)
+    offset = i.min + abs_max
+    return (sig.astype(dtype) - offset) / abs_max
+
+
 def normalize_unit(y):
     # ONLY SUPPORTS MONO
     norm_factor = np.abs(y).max()
